43570565c1789482ec361efc2dde250ff598b7a7
Matthias Harzer <matthias.harzer03@gmail.com>
2024-01-24 23:54:14 +0100

    Added api key to docker compose env

--- server/auth.py
+++ server/auth.py
@@ -14,6 +14,6 @@ def api_key_auth(request: Request) -> None:
 
     x_api_key = request.headers.get("X-API-Key")
 
-    if _API_KEY is not None and x_api_key != _API_KEY:
+    if _API_KEY and x_api_key != _API_KEY:
         raise HTTPException(status_code=401,
                             detail="Unauthorized. The requested resource is unavailable without a valid API key.")
